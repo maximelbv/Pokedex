cc8c53859477ed15b5d029d17b32b6c837bb5bb1
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2022-01-27 09:50:31 +0100

    add elements to page component & style them

--- src/views/Page/Page.js
+++ src/views/Page/Page.js
@@ -21,6 +21,15 @@ const Page = () => {
             .catch(err => {console.log(err)})
     }, [])
 
+    function convert(str) {
+            if (str.length >= 2) {
+                return str.slice(0, -1) + ',' + str.slice(-1);
+            } else if (str.length <= 1) {
+                return str.slice(0, -1) + '0,' + str.slice(-1);
+            }    
+    }
+
+    // if (pokemon.height) console.log(typeof pokemon.height.toString())
     return (
         <div className={style}>
 
@@ -28,13 +37,32 @@ const Page = () => {
 
             <div className='content'>
 
-                {pokemon.sprites && (<img className='image' src={pokemon.sprites.other.home.front_default} />)}
-
+                <div className={pokemon.types && `${pokemon.types[0].type.name}Bg imageBg`}>
+                    {pokemon.sprites && (<img className='image' src={pokemon.sprites.other.home.front_default} />)}
+                </div>
+                
                 <h1 className='name'>{pokemon.name && pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</h1>
 
-                <div className='infos'>
-                    <p>{pokemon.height}</p>
-                    {/* <p>{(pokemon.moves && pokemon.moves.length > 0) ? pokemon.moves[0].move.name : null}</p> */}
+                <h2 className='number'>{`#${pokemon.id}`}</h2>
+
+                <div className='types'>
+                    {pokemon.types && pokemon.types.map((t, j) => 
+                    <div key={j} className={`type ${t.type.name+'type'}`}>
+                    <div className={`typeImg ${t.type.name+'typeImg'}`}></div>
+                    <p className='typeName'>{t.type.name}</p>
+                    </div>
+                    )}
+                </div>
+
+                <div className='mensurations'>
+                    <div className='height'>
+                        <p>{`${pokemon.height && convert(pokemon.height.toString())} m`}</p>
+                        <p className='infoName'>Height</p>
+                    </div>
+                    <div className='weight'>
+                        <p>{`${pokemon.weight && convert(pokemon.weight.toString())} kg`}</p>
+                        <p className='infoName'>Weight</p>
+                    </div>
                 </div>
 
             </div>
